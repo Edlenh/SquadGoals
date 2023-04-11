7c550ee59ce96a0c6c3c54c61beae12cb09f5a72
Edward <herreraed93@gmail.com>
2023-04-10 19:48:17 -0700

    sign up login styles

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useLogin } from "../hooks/userLogin";
 
+
 const Login = ()=>{
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -11,31 +12,38 @@ const Login = ()=>{
        await login(email,password)
     }
     return (
-
-        <div>
+        <div className="login-wrapper">
+        <div className="login-bg">
         <form className ="login" onSubmit={submitHandler}>
-            <h2>Log In</h2>
-            <label>Email:</label>
+            <h2 className="login-main">Log In</h2>
+            {/* <label>Email</label> */}
             <input
+            placeholder="Email"
             type = "email"
             autoComplete= "off"
             onChange={(e)=> setEmail(e.target.value)}
             value={email}
             />
-            <label>Password:</label>
+            {/* <label>Password</label> */}
             <input
+            placeholder="Password"
             type ="password"
             autoComplete= "off"
             onChange={(e)=> setPassword(e.target.value)}
             value={password} 
             />
-            <button disabled={loading}>Login</button>
+            <button disabled={loading}>Submit</button>
             {error && <div className="error">{error}</div>}
         </form>
+        </div>
+        <footer className="form-footer">
+        <div className="form-footer-container">
+        <h1 className="form-foot">SQUAD <br></br> GOALS</h1>
+        <p className="form-p">Welcome Back!</p> 
+        </div>
+        </footer>
+        </div>
 
-        <h1 className="form-foot">SQUAD GOALS</h1>
-        <p>Welcome Back!</p>
-</div>
     )
 };
 export default Login
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -12,39 +12,47 @@ const SignUp = ()=>{
         await signup(email, username, password)
     }   
     return(<div>
+        <div className="signup-wrapper">
+        <div className="signup-bg">
         <form className ="signup" onSubmit={submitHandler}>
-        <h2>Sign Up</h2>
-        <label>Email:</label>
+        <h2 className="signup-main">Sign Up</h2>
+        {/* <label>Email</label> */}
         <input
+        placeholder="Email"
         type = "email"
         onChange={(e)=> setEmail(e.target.value)}
         value={email}
         />
         <br></br>
-         <label>Username:</label>
+         {/* <label>Username</label> */}
         <input
+        placeholder="Username"
         type ="text"
         onChange={(e)=> setUsername(e.target.value)}
         value={username} 
         />
         <br></br>
-        <label>Password:</label>
+        {/* <label>Password</label> */}
         <input
+        placeholder="Password"
         type ="password"
         onChange={(e)=> setPassword(e.target.value)}
         value={password} 
         />
-        <button disabled={loading}>Submit</button>
+        <button disabled={loading}>SUBMIT</button>
         {error && <div className="error">{error}</div>}
     </form>
+        </div>
         <section className="form-section">
-        <h1 className="form-foot">SQUAD GOALS</h1>
+        <div className="form-footer-container">
+        <h1 className="form-foot">SQUAD <br></br> GOALS</h1>
         <div className="ptag">
             <div className="pshadow"><small>
-                Getting it done, together</small></div>
+                Get it done, together</small></div>
+                </div>
         </div>
         </section>
-   
+    </div>
     </div>
 )
 };
